39befc8f3cc7f6d6df9384d963837638d4f88b27
Parva <parvadesai404@gmail.com>
2023-10-09 22:37:35 +0530

    Added the project

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody ball;
+    [SerializeField] private GameObject win, GameOver;
+    [SerializeField] private Text ScoreBoard;
+    [SerializeField] private int score; 
+    private int coinCount;
+
+    void Start()
+    {
+        if (ball == null)
+            ball = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 vel = new Vector3(-vertical, 0, horizontal);
+
+        ball.AddForce(vel * Time.deltaTime * 100f);
+
+        ScoreBoard.text = "" + score; 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("finishline"))
+        {
+            win.SetActive(true);
+            score += 50; 
+        }
+
+        else if(collision.gameObject.CompareTag("ground"))
+        {
+            GameOver.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+            coinCount++;
+            score = coinCount; 
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Player.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, Player.position + offset, Time.deltaTime * 5f);
+    }
+}
